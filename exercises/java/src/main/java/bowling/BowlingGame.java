b936749c3166872fef2e2ee2a3d9bfaa86520e9c
Sonia DAndreta <sonia.dandreta@autotrader.co.uk>
2025-05-14 14:16:41 +0100

    Refactored logic and variable names

--- exercises/java/src/main/java/bowling/BowlingGame.java
+++ exercises/java/src/main/java/bowling/BowlingGame.java
@@ -5,24 +5,18 @@ import java.util.Arrays;
 public class BowlingGame {
 
     public int totalScore(int[] gameResults) {
-        int result = 0;
+        int runningTotal = 0;
         for (int i = 0; i < gameResults.length; i += 2) {
-            result += gameResults[i];
-            result += gameResults[i + 1];
-            if (result == 10) {
-                result += gameResults[i + 2];
+            int frame = 0;
+            frame += gameResults[i];
+            frame += gameResults[i + 1];
+            if (frame == 10) {
+                frame += gameResults[i + 2];
             }
+            runningTotal += frame;
         }
-        return result;
+        return runningTotal;
     }
 }
-
-           /*int runningTotal = 0;
-           runningTotal = gameResults[i] + gameResults[i + 1];
-           if (runningTotal == 10) {
-              runningTotal += gameResults[i + 2];
-           }
-       }
-        return Arrays.stream(gameResults).sum();*/
 
 
