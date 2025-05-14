5240b3fbddd80e46307ce6578f4d5dc678d0ec4c
Sonia DAndreta <sonia.dandreta@autotrader.co.uk>
2025-05-14 12:24:47 +0100

    Refactor - remove redundant test

--- exercises/java/src/test/java/bowling/BowlingTest.java
+++ exercises/java/src/test/java/bowling/BowlingTest.java
@@ -5,18 +5,6 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class BowlingTest {
-    @Test
-    public void givenAllZero_returnZero() {
-        // Given
-        BowlingGame bowlingGame = new BowlingGame();
-        int[] gameResults = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-        // When
-        int result = bowlingGame.totalScore(gameResults);
-
-        // Then
-        assertThat(result).isZero();
-    }
 
     @Test
     public void givenScores_returnTotal() {
