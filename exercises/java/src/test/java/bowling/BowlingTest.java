2f104c1fcacd54a44774986097d581cd521569c8
Sonia DAndreta <sonia.dandreta@autotrader.co.uk>
2025-05-14 12:00:31 +0100

    Wrote test and method implemenation to return zero if all scores zero.

--- exercises/java/src/test/java/bowling/BowlingTest.java
+++ exercises/java/src/test/java/bowling/BowlingTest.java
@@ -2,9 +2,19 @@ package bowling;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class BowlingTest {
     @Test
-    public void renameMe() {
-        
+    public void givenAllZero_returnZero() {
+        // Given
+        BowlingGame bowlingGame = new BowlingGame();
+        int[] gameResults = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        // When
+        int result = bowlingGame.totalScore(gameResults);
+
+        // Then
+        assertThat(result).isZero();
     }
 }
